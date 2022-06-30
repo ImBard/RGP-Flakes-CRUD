a0b33e63926d29181edb907199fc52316290a9c2
ImBard <nicota1234@gmail.com>
2022-06-30 00:46:24 -0300

    :recycle: refactor: change interfaces a and search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,121 @@
 import { gql, useQuery } from "@apollo/client";
+import { MagnifyingGlass } from "phosphor-react";
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Character } from "../components/Character";
 import { Header } from "../components/Header";
 
-const GET_CHARACTER_QUERY = gql`
-    query {
-        characters {
-            name
-            class
-            age
-            gender
-            description
-            healthPower
-            attack
-            defense
-            characterId
-        }
+const GET_CHARACTERS_QUERY = gql`
+  query {
+    characters {
+      name
+      class
+      age
+      gender
+      description
+      healthPower
+      attack
+      defense
+      characterId
     }
+  }
 `
+// const GET_ONE_CHARACTER_QUERY = gql`
+//   query ($characterId: String!) {
+//     character(where: {characterId: $characterId}) {
+//       age
+//       attack
+//       class
+//       characterId
+//       defense
+//       description
+//       gender
+//       healthPower
+//       name
+//     }
+// }
+// `
 
-interface CharacterOne {
-    name: string;
-    class: string;
-    age: string;
-    gender: string;
-    description: string;
-    'health-power': string;
-    attack: string;
-    defense: string;
-    characterId: string;
+
+interface Characters {
+  name: string;
+  class: string;
+  age: string;
+  gender: string;
+  description: string;
+  healthPower: string;
+  attack: string;
+  defense: string;
+  characterId: string;
 }
+
+// interface OneCharacter {
+//   name: string;
+//   class: string;
+//   age: string;
+//   gender: string;
+//   description: string;
+//   'health-power': string;
+//   attack: string;
+//   defense: string;
+//   characterId: string;
+// }
 
 
 export function Home() {
 
-    const { data } = useQuery<{ characters: CharacterOne[] }>(GET_CHARACTER_QUERY)
-    console.log({ data })
+  const [list, setList] = useState(true);
 
+  const [searchId, setSearchId] = useState('');
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex flex-1 items-center justify-center">
-                {data?.characters.map(character => {
-                    return (
-                        <Character
-                            key={character.characterId}
-                            name={character.name}
-                            class={character.class}
-                            age={character.age}
-                            gender={character.gender}
-                            description={character.description}
-                            health-power={character["health-power"]}
-                            attack={character.attack}
-                            defense={character.defense}
-                        />
-                    )
-                })}
-            </main>
-        </div>
-    )
+  const { data } = useQuery<{ characters: Characters[] }>(GET_CHARACTERS_QUERY)
+
+  // const { data: char } = useQuery<{ character: OneCharacter }>(GET_ONE_CHARACTER_QUERY, {
+  //   variables: {
+  //     characterId: searchId
+  //   }
+  // })
+
+  const navigate = useNavigate()
+
+  function goCreate() {
+    navigate("/")
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header onclick={goCreate}/>
+      <main className="flex flex-1 flex-col items-center justify-center">
+        <form>
+          <label htmlFor="search" className="flex my-10 gap-4 items-center bg-gray-600 p-2 rounded ">
+            <button
+              className="p-2"
+              type="submit"
+            >
+              <MagnifyingGlass size={24} />
+            </button>
+
+            <input type="text" id="search" placeholder="Search a character" onChange={event => setSearchId(event.target.value)} className="p-2 bg-gray-900 border border-gray-400 rounded hover:bg-gray-600 max-w-sm w-96" />
+          </label>
+        </form>
+
+        {data?.characters.map(character => {
+            return (
+              <Character
+                key={character.characterId}
+                name={character.name}
+                class={character.class}
+                age={character.age}
+                gender={character.gender}
+                description={character.description}
+                health-power={character.healthPower}
+                attack={character.attack}
+                defense={character.defense}
+              />
+            )
+          })
+        }
+      </main>
+    </div>
+  )
 }

11d4d908d6bb40964d5dd8107b3f3158860ee040
ImBard <nicota1234@gmail.com>
2022-06-30 00:44:37 -0300

    :lipstick: feat: justify at center with mx-auto

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import logo from '/src/assets/MyFlakes_logo.png'
 export function Logo() {
   return (
     <img
-      className="w-32"
+      className="w-32 mx-auto"
       src={logo}
       alt=""
     />

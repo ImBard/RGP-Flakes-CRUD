3158410972e23c58618ec35bcc084c2dd33c9ae2
ImBard <nicota1234@gmail.com>
2022-06-30 00:44:00 -0300

    :sparkles: feat: new button on header, goHome or goCreate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+import { House } from "phosphor-react";
 import { Logo } from "./Logo";
 
-export function Header() {
+interface Button {
+    onclick: any;
+}
+
+export function Header(props: Button) {
     return (
-        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+        <header className="w-full py-5 flex justify-end bg-gray-700 border-b border-gray-600">
             <Logo />
+
+            <button className="mr-6" onClick={props.onclick}>
+                <House size={24}/>
+            </button>
         </header>
     )
 }

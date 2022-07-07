9a0ac4582e84f596ca073f96988cfe9feede8004
ImBard <nicota1234@gmail.com>
2022-07-06 23:01:04 -0300

    :sparkles: feat: Add new route to onecharacter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import { OneCharacter } from "./components/OneCharacter";
 import { Create } from "./pages/Create";
 import { Home } from "./pages/Home";
+import { MyCharacter } from "./pages/MyCharacter";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Create />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/MyCharacter/:slug" element={<MyCharacter />} />
     </Routes>
   )
 }

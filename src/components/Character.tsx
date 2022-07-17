0112408c4d77317dea3f6c6f9a831d3177d7039e
ImBard <nicota1234@gmail.com>
2022-07-17 01:09:32 -0300

    :lipstick: feat: Correct style in card oneCharacter

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -17,10 +17,10 @@ interface GetCharacter {
 export function Character(props: GetCharacter) {
 
   return (
-    <div className="bg-gray-600 flex  gap-5 justify-center p-4 rounded border border-gray-500 my-10 max-w-lg w-[512px] active:bg-gray-700 transition-colors">
+    <div className="bg-gray-600 flex  gap-5 p-4 rounded border border-gray-500 my-10 max-w-lg w-[512px] active:bg-gray-700 transition-colors">
       <div className='flex flex-col'>
         <img
-          className='h-40 border border-myGreen-900 rounded mb-4'
+          className='h-40 border border-myGreen-900 rounded mb-4 max-w-[160px]'
           src={defaultCharacter}
           alt=""
         />
@@ -34,15 +34,15 @@ export function Character(props: GetCharacter) {
       </div>
 
       <div className='flex flex-col'>
-        <div className='grid grid-cols-2'>
-          <span className='p-2 font-bold'>Name: <span className='font-normal'>{props.name}</span></span>
-          <span className='p-2 font-bold'>Class: <span className='font-normal'>{props.class}</span></span>
-          <span className='p-2 font-bold'>Age: <span className='font-normal'>{props.age}</span></span>
-          <span className='p-2 font-bold'>Gender: <span className='font-normal'>{props.gender}</span></span>
+        <div className='grid grid-cols-2 w-72'>
+          <span className='p-2 font-bold flex flex-col'>Name: <span className='font-normal'>{props.name}</span></span>
+          <span className='p-2 font-bold flex flex-col'>Class: <span className='font-normal'>{props.class}</span></span>
+          <span className='p-2 font-bold flex flex-col'>Age: <span className='font-normal'>{props.age}</span></span>
+          <span className='p-2 font-bold flex flex-col'>Gender: <span className='font-normal'>{props.gender}</span></span>
         </div>
         <div className='flex flex-col max-w-sm'>
           <h1 className='font-bold text-lg'>Description</h1>
-          <span className=' rounded p-2 text-gray-300 bg-gray-700'>
+          <span className=' rounded p-2 text-gray-300 bg-gray-700 w-full'>
             {props.description}
           </span>
         </div>

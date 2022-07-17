c3ff5cd167d563e1aecdf83e5db3a77c2364d70f
ImBard <nicota1234@gmail.com>
2022-07-17 01:12:15 -0300

    :bug: fix: Search charcter by name in lower or upperCase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Character } from "../components/Character";
 import { Header } from "../components/Header";
+import { client } from "../lib/apollo";
 
 const GET_CHARACTERS_QUERY = gql`
   query {
-    characters {
+    characters(first: 100) {
       name
       class
       age
@@ -20,23 +21,6 @@ const GET_CHARACTERS_QUERY = gql`
     }
   }
 `
-// const GET_ONE_CHARACTER_QUERY = gql`
-//   query ($characterId: String!) {
-//     character(where: {characterId: $characterId}) {
-//       age
-//       attack
-//       class
-//       characterId
-//       defense
-//       description
-//       gender
-//       healthPower
-//       name
-//     }
-// }
-// `
-
-
 interface Characters {
   name: string;
   class: string;
@@ -49,19 +33,6 @@ interface Characters {
   characterId: string;
 }
 
-// interface OneCharacter {
-//   name: string;
-//   class: string;
-//   age: string;
-//   gender: string;
-//   description: string;
-//   'health-power': string;
-//   attack: string;
-//   defense: string;
-//   characterId: string;
-// }
-
-
 export function Home() {
 
   const [list, setList] = useState(true);
@@ -70,39 +41,23 @@ export function Home() {
 
   const { data } = useQuery<{ characters: Characters[] }>(GET_CHARACTERS_QUERY)
 
-  // const { data: char } = useQuery<{ character: OneCharacter }>(GET_ONE_CHARACTER_QUERY, {
-  //   variables: {
-  //     characterId: searchId
-  //   }
-  // })
-  console.log(data)
-  const navigate = useNavigate()
+  client.refetchQueries({
+    include: [GET_CHARACTERS_QUERY],
+  });
 
-  function goCreate() {
-    navigate("/")
-  }
-
-  const search = () => setList(false);
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header link={"/"} />
       <main className="flex flex-1 flex-col items-center justify-center">
         <label htmlFor="search" className="flex my-10 gap-4 items-center bg-gray-600 p-2 rounded ">
-          <button
-            onClick={search}
-            className="p-2"
-            type="submit"
-          >
-            <MagnifyingGlass size={24} />
-          </button>
-
+            <MagnifyingGlass size={24} style={{margin: '8px'}} />
           <input type="text" id="search" placeholder="Search a character" onChange={event => setSearchId(event.target.value)} className="p-2 bg-gray-900 border border-gray-400 rounded hover:bg-gray-600 max-w-sm w-96" />
         </label>
         {/* =========== SÓ ESTÁ BUSCANDO NOVOS PERSONAGENS QUANDO ATUALIZA A TELA(F5) */}
         {
           data?.characters.map(character => {
-            if (character.name.match(searchId) || character.characterId.match(searchId)) {
+            if (character.name.toLowerCase().match(searchId) || character.characterId.match(searchId)) {
               return (
                 <Link to={`/MyCharacter/${character.characterId}`}>
                   <Character
